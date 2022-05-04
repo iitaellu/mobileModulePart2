c4acb9deaf74139580f8cac869666be7987ccb5d
iitaellu <iitaellu@hotmail.com>
2022-05-04 17:40:17 +0300

    added two buttons. Other send information to other view and other opens google

--- app/src/main/java/com/example/mobilemodulepart2/MainActivity.java
+++ app/src/main/java/com/example/mobilemodulepart2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mobilemodulepart2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attempts to lounch an activity within our own app
+        Button secondActivityBtn = (Button) findViewById(R.id.secondAct);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("org.mentorschool.quicklauncher.SOMETHING", "Hello World!!");
+                startActivity(startIntent);
+            }
+        });
+
+        // Attempt to launch an activity outside our app
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) == null){
+                    startActivity(gotoGoogle);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mobilemodulepart2/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobilemodulepart2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        if (getIntent().hasExtra("org.mentorschool.quicklauncher.SOMETHING")) {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            String text = getIntent().getExtras().getString("org.mentorschool.quicklauncher.SOMETHING");
+            tv.setText(text);
+        }
+    }
+}
